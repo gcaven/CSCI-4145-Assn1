3e6f7a9e9557cbb15f3f12f40e3db5ed6121c4de
Geoffrey Caven <geoffrey@bravenew.world>
2017-03-20 22:41:33 -0300

    add assn to repo

--- server.js
+++ server.js
@@ -15,22 +15,22 @@ function convert(unit, value, response) {
   if (typeof value !== 'number') {
     return response.status(400).send();
   }
-  if (unit == 'celcius') {
+  if (unit == 'celsius') {
     //C to F
     //Multiply by 9, then divide by 5, then add 32
     var num = parseFloat(value);
     num = num*9;
     num = num/5;
     num = num+32;
-    return response.send({"unit":"farenheit","value":num});
-  } else if (unit == 'farenheit') {
+    return response.send({"unit":"fahrenheit","value":num});
+  } else if (unit == 'fahrenheit') {
     //F to C
     //Deduct 32, then multiply by 5, then divide by 9
     var num = parseFloat(value);
     num = num-32;
     num = num*5;
     num = num/9;
-    return response.send({"unit":"celcius","value":num});
+    return response.send({"unit":"celsius","value":num});
   } else {
     return response.status(400).send();
   }
